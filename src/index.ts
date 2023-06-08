4e156876679d9219192e9900e477a81690a22ef5
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-06-07 22:36:27 -0400

    Change: https to http

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,15 @@
 import express from "express";
 import cors from "cors";
 import { config } from "dotenv";
-import https from "https";
-import { options } from "./config/ssl_config";
 // Routes
 import userRoutes from './routes/user_routes';
 
 config();
 
-const app = express();
-app.use(cors(), express.json());
+const server = express();
+server.use(cors(), express.json());
 
-app.use(userRoutes);
-
-const server = https.createServer(options, app);
-
+server.use(userRoutes);
 
 server.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}`);
