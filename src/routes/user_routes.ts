12b685d2acbb6af1fc6753d519a4defc452804e2
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-06-26 23:06:30 -0400

    Add: New route deleteUser

--- src/routes/user_routes.ts
+++ src/routes/user_routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
-import { getAllUsers, getUserById, createUser } from "../controllers/user_controller";
+import { getAllUsers, getUserById, createUser, deleteUser } from "../controllers/user_controller";
 
 const router = Router();
 
 router.get("/users", getAllUsers);
 router.get("/users/:id", getUserById);
 router.post("/users", createUser);
+router.delete("/users/:id", deleteUser);
 
 export default router;
