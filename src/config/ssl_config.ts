28f6bc42df7bc4b5fdc3b335a197d1d211545b67
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-06-26 22:43:47 -0400

    Change: ssl_config deleted

--- src/config/ssl_config.ts
+++
@@ -1,9 +0,0 @@
-// Lib
-import fs from "fs";
-import { config } from "dotenv";
-config();
-
-export const options = {
-  key: fs.readFileSync(process.env.KEY_PATH!),
-  cert: fs.readFileSync(process.env.CERT_PATH!)
-};
