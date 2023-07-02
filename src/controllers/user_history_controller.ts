dc69eb6ff491f2a3fd0bb1c22035622dbe9796f0
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-07-01 22:48:24 -0400

    Change: Insert the record in user_history with the calculated previous_balance

--- src/controllers/user_history_controller.ts
+++ src/controllers/user_history_controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import db from "../config/data_base";
+import { RowDataPacket } from "mysql2";
 // Utils
 import getCurrentDateTimeInDominicanRepublic from "../utils/getCurrentDateTime";
 
@@ -18,6 +19,17 @@ export const getAllUserHistory = async (_req: Request, res: Response) => {
 export const addRecordToHistory = async (req: Request, res: Response) => {
 	try {
 		const connection = await db.getConnection();
+
+		// Get the current balance of the user
+		const [balanceRows] = (await connection.query(
+			"SELECT SUM(CASE WHEN transfer_type = 'income' THEN amount ELSE 0 END) - SUM(CASE WHEN transfer_type = 'spent' THEN amount ELSE 0 END) AS balance FROM user_history WHERE user_id = ?",
+			[req.body.user_id]
+		)) as RowDataPacket[];
+
+		// Extract the balance from the query result
+		const currentBalance = balanceRows[0]?.balance || 0;
+
+		// Insert the record in user_history with the calculated previous_balance
 		const [rows] = await connection.query(
 			"INSERT INTO user_history (user_id, creation_date, transfer_type, amount, previous_balance, description, expenses_type) VALUES (?, ?, ?, ?, ?, ?, ?)",
 			[
@@ -25,11 +37,12 @@ export const addRecordToHistory = async (req: Request, res: Response) => {
 				getCurrentDateTimeInDominicanRepublic(),
 				req.body.transfer_type,
 				req.body.amount,
-				req.body.previous_balance,
+				currentBalance,
 				req.body.description,
 				req.body.expenses_type,
 			]
 		);
+
 		connection.release();
 		res.json(rows);
 	} catch (err) {
