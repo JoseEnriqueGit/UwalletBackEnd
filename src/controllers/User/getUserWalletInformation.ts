5eb86d574f9dc887468a636d78df1b94f8f3b6d6
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-08-08 21:51:33 -0400

    Refactoring: Improved User Wallet Information Retrieval
    In this commit, a significant refactoring has been performed on the `getUserWalletInformation` function to enhance code efficiency and readability. The following changes have been implemented:
    
    - Reusable SQL queries have been defined to fetch user wallet information and main wallet balance.
    - The code has been restructured to reduce redundancy and enhance the flow of control.
    - Improved database connection management using the `execute` method instead of `query`.
    
    Furthermore, added clarity to the code through the use of descriptive variable names and simplified logic for determining whether the user has a second wallet.
    
    These changes contribute to cleaner and more efficient code, providing a solid foundation for future enhancements to user wallet functionality.

--- src/controllers/User/getUserWalletInformation.ts
+++ src/controllers/User/getUserWalletInformation.ts
@@ -2,29 +2,35 @@ import { Request, Response } from "express";
 import { RowDataPacket } from "mysql2";
 import db from "../../config/data_base";
 
-export const getUserWalletInformation = async (req: Request, res: Response) => {
-	try {
-		const connection = await db.getConnection();
+const GET_WALLETS_INFO_QUERY =
+	"SELECT id, currency, is_main_wallet, is_second_wallet FROM users_wallets WHERE user_id = ?";
+const GET_MAIN_WALLET_BALANCE_QUERY =
+	"SELECT SUM(CASE WHEN transfer_type = 'income' THEN amount ELSE 0 END) - SUM(CASE WHEN transfer_type = 'spent' THEN amount ELSE 0 END) AS balance FROM user_history WHERE user_id = ? AND wallet_id = ?";
 
+export const getUserWalletInformation = async (req: Request, res: Response) => {
+	const connection = await db.getConnection();
+	try {
 		// Get the user's wallets info from users_wallets table
-		const [walletsRows] = await connection.query(
-			"SELECT id, currency, is_main_wallet, is_second_wallet FROM users_wallets WHERE user_id = ?",
+		const [walletsRows] = await connection.execute<RowDataPacket[]>(
+			GET_WALLETS_INFO_QUERY,
 			[req.params.id]
 		);
 
 		if (Array.isArray(walletsRows) && walletsRows.length > 0) {
 			let mainWalletBalance = 0;
 			let hasSecondWallet = false;
+			let currency = "";
 			let secondWalletCurrency = "";
 
-			for (const walletRow of walletsRows as RowDataPacket[]) {
+			for (const walletRow of walletsRows) {
 				if (walletRow.is_main_wallet) {
 					const mainWalletId = walletRow.id;
-					const [mainWalletRows] = await connection.query<RowDataPacket[]>(
-						"SELECT SUM(CASE WHEN transfer_type = 'income' THEN amount ELSE 0 END) - SUM(CASE WHEN transfer_type = 'spent' THEN amount ELSE 0 END) AS balance FROM user_history WHERE user_id = ? AND wallet_id = ?",
+					const [mainWalletRows] = await connection.execute<RowDataPacket[]>(
+						GET_MAIN_WALLET_BALANCE_QUERY,
 						[req.params.id, mainWalletId]
 					);
 					mainWalletBalance = mainWalletRows[0]?.balance || 0;
+					currency = walletRow.currency;
 				}
 
 				if (walletRow.is_second_wallet) {
@@ -33,28 +39,26 @@ export const getUserWalletInformation = async (req: Request, res: Response) => {
 				}
 			}
 
-			connection.release();
+			const response: {
+				balance: number;
+				currency: string;
+				hasSecondWallet: boolean;
+				secondWalletCurrency: string;
+			} = {
+				balance: mainWalletBalance,
+				currency: currency,
+				hasSecondWallet: hasSecondWallet,
+				secondWalletCurrency: secondWalletCurrency,
+			};
 
-			if (mainWalletBalance !== undefined) {
-				const response: {
-					balance: number;
-					hasSecondWallet: boolean;
-					secondWalletCurrency: string;
-				} = {
-					balance: mainWalletBalance,
-					hasSecondWallet: hasSecondWallet,
-					secondWalletCurrency: secondWalletCurrency,
-				};
-				res.json(response);
-			} else {
-				res.status(404).json({ error: "Balance not found" });
-			}
+			res.json(response);
 		} else {
-			connection.release();
 			res.status(404).json({ error: "Wallets not found" });
 		}
 	} catch (err) {
 		console.error(err);
 		res.status(500).json({ error: "Internal server error" });
+	} finally {
+		connection.release();
 	}
 };
