4381dc996b65cf6d77bb30a6095e40111ba33aca
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-08-09 23:06:07 -0400

    Change: Enhanced Database Management
    This commit introduces improvements to the database configuration and management. The following changes have been made:
    
    - Added logging capabilities using Winston to track errors and transactions.
    - Replaced the `getConnection` method with more specialized methods: `query` for executing queries and `transaction` for managing transactions.
    - The `close` method has been added to ensure proper closure of the database pool.
    
    These enhancements provide better error handling, improved transaction management, and enhanced database connectivity.

--- src/config/data_base.ts
+++ src/config/data_base.ts
@@ -1,21 +1,66 @@
 import mysql from "mysql2/promise";
 import { config } from "dotenv";
+import { createLogger, transports } from "winston";
+import { DatabaseError, TransactionError } from "./errors";
 
 config();
 
+const logger = createLogger({
+	transports: [
+		new transports.Console(),
+		new transports.File({ filename: "error.log", level: "error" }),
+	],
+});
+
 class Database {
-  private pool: mysql.Pool;
-  
-  constructor() {
-    this.pool = mysql.createPool(process.env.DATABASE_URL!);
-  }
-  
-  public getConnection(): Promise<mysql.PoolConnection> {
-    return this.pool.getConnection();
-  }
+	private pool: mysql.Pool;
+
+	constructor() {
+		this.pool = mysql.createPool(process.env.DATABASE_URL!);
+	}
+
+	public async query(sql: string, values?: any): Promise<any> {
+		const conn = await this.pool.getConnection();
+		try {
+			const [rows] = await conn.query(sql, values);
+			return rows;
+		} catch (err) {
+			const errorMessage = `Error executing query: ${sql}`;
+			logger.error(errorMessage, { error: err });
+			throw new DatabaseError(errorMessage, err as Error);
+		} finally {
+			conn.release();
+		}
+	}
+
+	public async transaction<T>(
+		callback: (conn: mysql.PoolConnection) => Promise<T>
+	): Promise<T> {
+		const conn = await this.pool.getConnection();
+		await conn.beginTransaction();
+		try {
+			const result = await callback(conn);
+			await conn.commit();
+			return result;
+		} catch (err) {
+			const errorMessage = "Transaction error";
+			logger.error(errorMessage, { error: err });
+			await conn.rollback();
+			throw new TransactionError(errorMessage, err as Error);
+		} finally {
+			conn.release();
+		}
+	}
+
+	public async close(): Promise<void> {
+		await this.pool.end();
+	}
+	// Delete this
+	public getConnection(): Promise<mysql.PoolConnection> {
+		return this.pool.getConnection();
+	}
 }
 
 const db = new Database();
 
 export default db;
-
