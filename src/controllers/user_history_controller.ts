ea47585a6c0e38e62fbd926e5ba847a4117cd03c
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-08-10 22:16:56 -0400

    Change: Streamlining User History Management Functions
    In this update, the user history management functions have undergone a streamlined optimization to enhance code clarity and efficiency. The changes implemented include:
    
    1. Replacement of manual database connection handling with the use of the `db.query` function for executing queries in a more concise and comprehensible manner.
    
    2. Simplified retrieval of the main wallet ID from the `users_wallets` table using `db.query` and safer handling of query results.
    
    3. Optimization of balance calculation by utilizing `db.query` and enhancing readability.
    
    4. Improved handling of insufficiency of funds scenario with concise conditional checks.
    
    5. Integration of the `getCurrentDateTimeInDominicanRepublic` utility function for consistent timestamp generation.
    
    These alterations are aimed at maintaining a cleaner, more efficient, and coherent codebase while ensuring the proper functioning of the user history management operations.

--- src/controllers/user_history_controller.ts
+++ src/controllers/user_history_controller.ts
@@ -6,9 +6,7 @@ import getCurrentDateTimeInDominicanRepublic from "../utils/getCurrentDateTime";
 
 export const getAllUserHistory = async (_req: Request, res: Response) => {
 	try {
-		const connection = await db.getConnection();
-		const [rows] = await connection.query("SELECT * FROM user_history");
-		connection.release();
+		const rows = await db.query("SELECT * FROM user_history");
 		res.json(rows);
 	} catch (err) {
 		console.error(err);
@@ -18,43 +16,34 @@ export const getAllUserHistory = async (_req: Request, res: Response) => {
 
 export const addRecordToHistory = async (req: Request, res: Response) => {
 	try {
-		const connection = await db.getConnection();
-
-		// Get the main wallet ID of the user from users_wallets table
-		const [mainWalletRow] = (await connection.query(
+		const mainWalletRow = (await db.query(
 			"SELECT id FROM users_wallets WHERE user_id = ? AND is_main_wallet = ?",
 			[req.body.user_id, true]
 		)) as RowDataPacket[];
 
 		if (!Array.isArray(mainWalletRow) || mainWalletRow.length === 0) {
-			connection.release();
 			res.status(404).json({ error: "Main wallet not found" });
 			return;
 		}
 
-		const mainWalletId = mainWalletRow[0].id;
+		const mainWalletId = mainWalletRow[0]?.id;
 
-		// Get the current balance of the user
-		const [balanceRows] = (await connection.query(
+		const balanceRows = (await db.query(
 			"SELECT SUM(CASE WHEN transfer_type = 'income' THEN amount ELSE 0 END) - SUM(CASE WHEN transfer_type = 'spent' THEN amount ELSE 0 END) AS balance FROM user_history WHERE user_id = ? AND wallet_id = ?",
 			[req.body.user_id, mainWalletId]
 		)) as RowDataPacket[];
 
-		// Extract the balance from the query result
 		const currentBalance = balanceRows[0]?.balance || 0;
 
-		// Check if the transfer_type is 'spent' and if the amount exceeds the current balance
 		if (
 			req.body.transfer_type === "spent" &&
 			req.body.amount > currentBalance
 		) {
-			connection.release();
 			res.status(400).json({ error: "Insufficient funds" });
 			return;
 		}
 
-		// Insert the record in user_history with the calculated previous_balance and wallet_id
-		const [rows] = await connection.query(
+		const rows = await db.query(
 			"INSERT INTO user_history (user_id, wallet_id, creation_date, transfer_type, amount, previous_balance, description, expenses_type) VALUES (?, ?, ?, ?, ?, ?, ?, COALESCE(?, 'UNDEFINED'))",
 			[
 				req.body.user_id,
@@ -68,7 +57,6 @@ export const addRecordToHistory = async (req: Request, res: Response) => {
 			]
 		);
 
-		connection.release();
 		res.json(rows);
 	} catch (err) {
 		console.error(err);
