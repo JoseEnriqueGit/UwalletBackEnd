44b9be349c921bef355f1d6b7f1b5ec5aaba90bb
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-08-10 22:14:26 -0400

    Change: Refactoring of User Handling Functions
    In this update, a significant refactoring has been performed on the user handling functions to enhance the code structure and efficiency. The changes made include:
    
    1. Replacing manual database connection management with the use of the `db.query` function for executing queries in a more simplified and readable manner.
    
    2. Utilizing the `db.transaction` function to group transaction-related operations, which improves robustness and error handling in case of failures.
    
    3. Streamlining the retrieval of `wallet_id` using destructuring and safer handling of query results.
    
    4. Code optimization by eliminating query duplication and reducing complexity.
    
    These changes aim to maintain cleaner, more efficient, and easily maintainable code, enhancing scalability and reliability of database operations related to users.

--- src/controllers/user_controller.ts
+++ src/controllers/user_controller.ts
@@ -6,9 +6,7 @@ import getCurrentDateTimeInDominicanRepublic from "../utils/getCurrentDateTime";
 
 export const getAllUsers = async (_req: Request, res: Response) => {
 	try {
-		const connection = await db.getConnection();
-		const [rows] = await connection.query("SELECT * FROM users");
-		connection.release();
+		const rows = await db.query("SELECT * FROM users");
 		res.json(rows);
 	} catch (err) {
 		console.error(err);
@@ -18,11 +16,9 @@ export const getAllUsers = async (_req: Request, res: Response) => {
 
 export const getUserById = async (req: Request, res: Response) => {
 	try {
-		const connection = await db.getConnection();
-		const [rows] = await connection.query("SELECT * FROM users WHERE id = ?", [
+		const rows = await db.query("SELECT * FROM users WHERE id = ?", [
 			req.params.id,
 		]);
-		connection.release();
 		res.json(rows);
 	} catch (err) {
 		console.error(err);
@@ -31,74 +27,45 @@ export const getUserById = async (req: Request, res: Response) => {
 };
 
 export const createUser = async (req: Request, res: Response) => {
-	const connection = await db.getConnection();
 	try {
 		const user_id = req.body.data.id;
 		const email = req.body.data.email_addresses[0].email_address;
 		const username = req.body.data.username;
 
-		// Start a transaction
-		await connection.beginTransaction();
+		await db.transaction(async (conn) => {
+			await conn.query(
+				"INSERT INTO users (id, username, email) VALUES (?, ?, ?)",
+				[user_id, username, email]
+			);
 
-		// Insert the user in the users table
-		await connection.query(
-			"INSERT INTO users (id, username, email) VALUES (?, ?, ?)",
-			[user_id, username, email]
-		);
+			await conn.query(
+				"INSERT INTO users_wallets (user_id, is_main_wallet) VALUES (?, ?)",
+				[user_id, true]
+			);
 
-		// Insert the user's wallet in the users_wallets table
-		await connection.query(
-			"INSERT INTO users_wallets (user_id, is_main_wallet) VALUES (?, ?)",
-			[user_id, true]
-		);
+			const [walletRow] = (await conn.query(
+				"SELECT id FROM users_wallets WHERE user_id = ? AND is_main_wallet = ?",
+				[user_id, true]
+			)) as RowDataPacket[];
 
-		// Get the wallet_id for the main wallet
-		const [walletRow] = (await connection.query(
-			"SELECT id FROM users_wallets WHERE user_id = ? AND is_main_wallet = ?",
-			[user_id, true]
-		)) as RowDataPacket[];
+			const wallet_id = walletRow[0]?.id;
 
-		const wallet_id = walletRow[0].id;
+			await conn.query(
+				"INSERT INTO user_history (user_id, wallet_id, creation_date, transfer_type, amount, previous_balance, description, expenses_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+				[
+					user_id,
+					wallet_id,
+					getCurrentDateTimeInDominicanRepublic(),
+					"income",
+					0,
+					0,
+					"Start using",
+					"UNDEFINED",
+				]
+			);
+		});
 
-		// Start the user history with a 0 balance and the wallet_id
-		await connection.query(
-			"INSERT INTO user_history (user_id, wallet_id, creation_date, transfer_type, amount, previous_balance, description, expenses_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-			[
-				user_id,
-				wallet_id,
-				getCurrentDateTimeInDominicanRepublic(),
-				"income",
-				0,
-				0,
-				"Start using",
-				"UNDEFINED",
-			]
-		);
-
-		// Commit the transaction
-		await connection.commit();
-
-		connection.release();
 		res.json({ success: true });
-	} catch (err) {
-		console.error(err);
-
-		// Rollback the transaction in case of an error
-		await connection.rollback();
-
-		res.status(500).json({ error: "Internal server error" });
-	}
-};
-
-export const updateUser = async (req: Request, res: Response) => {
-	try {
-		const connection = await db.getConnection();
-		const [rows] = await connection.query(
-			"UPDATE users SET name = ?, email = ?, password = ? WHERE id = ?",
-			[req.body.name, req.body.email, req.body.password, req.params.id]
-		);
-		connection.release();
-		res.json(rows);
 	} catch (err) {
 		console.error(err);
 		res.status(500).json({ error: "Internal server error" });
@@ -106,37 +73,22 @@ export const updateUser = async (req: Request, res: Response) => {
 };
 
 export const deleteUser = async (req: Request, res: Response) => {
-	const connection = await db.getConnection();
 	try {
-		// Start a transaction
-		await connection.beginTransaction();
+		await db.transaction(async (conn) => {
+			await conn.query("DELETE FROM user_history WHERE user_id = ?", [
+				req.params.id,
+			]);
+			await conn.query("DELETE FROM user_wishes WHERE user_id = ?", [
+				req.params.id,
+			]);
+			const [deleteRows] = await conn.query("DELETE FROM users WHERE id = ?", [
+				req.params.id,
+			]);
 
-		// Delete records from the user_history table
-		await connection.query("DELETE FROM user_history WHERE user_id = ?", [
-			req.params.id,
-		]);
-
-		// Delete records from the user_wishes table
-		await connection.query("DELETE FROM user_wishes WHERE user_id = ?", [
-			req.params.id,
-		]);
-
-		// Delete user from the users table
-		const [rows] = await connection.query("DELETE FROM users WHERE id = ?", [
-			req.params.id,
-		]);
-
-		// Commit the transaction
-		await connection.commit();
-
-		connection.release();
-		res.json(rows);
+			res.json(deleteRows);
+		});
 	} catch (err) {
 		console.error(err);
-
-		// Rollback the transaction in case of an error
-		await connection.rollback();
-
 		res.status(500).json({ error: "Internal server error" });
 	}
 };
