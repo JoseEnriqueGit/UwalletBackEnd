1e6d0195c5f3257107db94eda5b2c634bea8e65b
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-08-10 22:19:35 -0400

    Change: Enhance Wallet Creation Process and Transaction Handling
    In this commit, improvements have been made to the wallet creation process and the way transactions are handled for greater efficiency and reliability. The changes made include:
    
    1. Implementation of a database transaction using the `db.transaction` function to ensure atomicity and consistency during wallet creation and history record insertion.
    
    2. Adoption of the `ResultSetHeader` type for query results to accurately access the `insertId` property and retrieve the newly created wallet's ID.
    
    4. Refactoring of the code for clarity and conciseness.
    
    These updates aim to enhance the overall robustness and maintainability of the wallet creation process while providing a seamless experience for users.

--- src/controllers/user_wallets_controller.ts
+++ src/controllers/user_wallets_controller.ts
@@ -1,16 +1,15 @@
 import { Request, Response } from "express";
-import { OkPacket } from "mysql2"; // Replace with your actual import
 import db from "../config/data_base";
+import { ResultSetHeader } from "mysql2";
 // Utils
 import getCurrentDateTimeInDominicanRepublic from "../utils/getCurrentDateTime";
 
-
 export const createNewWallet = async (req: Request, res: Response) => {
-	const user_id = req.body.user_id; // Assuming you get the user ID from the request params
-	const currency = req.body.currency; // Get the currency from the request body
+	const user_id = req.body.user_id;
+	const currency = req.body.currency;
 	const amount = req.body.amount;
-	const is_main_wallet = req.body.is_main_wallet | 0; // Get the is_main_wallet from the request body
-	const is_second_wallet = req.body.is_second_wallet | 0; // Get the is_second_wallet from the request body
+	const is_main_wallet = req.body.is_main_wallet | 0;
+	const is_second_wallet = req.body.is_second_wallet | 0;
 
 	if (is_main_wallet && is_second_wallet) {
 		return res
@@ -19,30 +18,25 @@ export const createNewWallet = async (req: Request, res: Response) => {
 	}
 
 	try {
-		const connection = await db.getConnection();
-		await connection.beginTransaction();
+		await db.transaction(async (conn) => {
+			// Insert new wallet into users_wallets
+			const insertResult = await conn.query(
+				"INSERT INTO users_wallets (user_id, currency, is_main_wallet, is_second_wallet) VALUES (?, ?, ?, ?)",
+				[user_id, currency, is_main_wallet, is_second_wallet]
+			);
 
-		// Insert new wallet into users_wallets
-		const [insertResult] = await connection.query(
-			"INSERT INTO users_wallets (user_id, currency, is_main_wallet, is_second_wallet) VALUES (?, ?, ?, ?)",
-			[user_id, currency, is_main_wallet, is_second_wallet]
-		);
+			const wallet_id = (insertResult[0] as ResultSetHeader).insertId;
 
-		const wallet_id = (insertResult as OkPacket).insertId;
+			// Insert record into user_history
+			await conn.query(
+				"INSERT INTO user_history (user_id, wallet_id, creation_date, transfer_type, amount, previous_balance, description, expenses_type) VALUES (?, ?, ?, 'income', ?, 0, 'New wallet', 'UNDEFINED')",
+				[user_id, wallet_id, getCurrentDateTimeInDominicanRepublic(), amount]
+			);
 
-		// Insert record into user_history
-		await connection.query(
-			"INSERT INTO user_history (user_id, wallet_id, creation_date, transfer_type, amount, previous_balance, description, expenses_type) VALUES (?, ?, ?, 'income', ?, 0, 'New wallet', 'UNDEFINED')",
-			[user_id, wallet_id, getCurrentDateTimeInDominicanRepublic(), amount]
-		);
-
-		await connection.commit();
-		connection.release();
-
-		res.json({ message: "New wallet created successfully", wallet_id });
+			res.json({ message: "New wallet created successfully", wallet_id });
+		});
 	} catch (error) {
 		console.error(error);
 		res.status(500).json({ error: "Internal server error" });
 	}
 };
-
