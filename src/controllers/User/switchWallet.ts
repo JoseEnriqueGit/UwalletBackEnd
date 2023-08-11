db781c12d209a23c06bfd9b91681af8e524c494a
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-08-10 22:24:04 -0400

    Change: Optimize Wallet Switching Logic and Database Interaction
    This commit introduces enhancements to the wallet switching logic and the way database interactions are handled, resulting in improved efficiency and maintainability. The modifications made include:
    
    1. Adoption of the `db.query` function to execute queries, simplifying code and enhancing readability.
    
    2. Utilization of the `db.transaction` function for encapsulating the wallet update operations within a transaction, ensuring data integrity and consistency.
    
    3. Streamlined assignment of wallet IDs using the `mainWalletRows` and `secondWalletRows` query results.
    
    4. Refinement of error handling mechanisms and consistent logging of errors.
    
    These changes aim to provide a more efficient and robust approach to switching wallets while maintaining a clean and organized codebase.

--- src/controllers/User/switchWallet.ts
+++ src/controllers/User/switchWallet.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import { RowDataPacket } from "mysql2";
 import db from "../../config/data_base";
 
 const FIND_MAIN_WALLET_QUERY =
@@ -13,41 +12,28 @@ const UPDATE_SECOND_WALLET_QUERY =
 
 export const switchWallet = async (req: Request, res: Response) => {
 	const user_id = req.params.user_id;
-	const connection = await db.getConnection();
 
 	try {
-		await connection.beginTransaction();
-
-		// Find the main wallet ID for the user
-		const [mainWalletRows] = await connection.execute<RowDataPacket[]>(
-			FIND_MAIN_WALLET_QUERY,
-			[user_id]
-		);
-
-		// Find the second wallet ID for the user
-		const [secondWalletRows] = await connection.execute<RowDataPacket[]>(
-			FIND_SECOND_WALLET_QUERY,
-			[user_id]
-		);
+		const mainWalletRows = await db.query(FIND_MAIN_WALLET_QUERY, [user_id]);
+		const secondWalletRows = await db.query(FIND_SECOND_WALLET_QUERY, [
+			user_id,
+		]);
 
 		const mainWalletId = mainWalletRows[0]?.id;
 		const secondWalletId = secondWalletRows[0]?.id;
 
 		if (mainWalletId && secondWalletId) {
-			// Update flags for the main wallet and the second wallet
-			await connection.execute(UPDATE_MAIN_WALLET_QUERY, [mainWalletId]);
-			await connection.execute(UPDATE_SECOND_WALLET_QUERY, [secondWalletId]);
+			await db.transaction(async (conn) => {
+				await conn.query(UPDATE_MAIN_WALLET_QUERY, [mainWalletId]);
+				await conn.query(UPDATE_SECOND_WALLET_QUERY, [secondWalletId]);
+			});
 
-			await connection.commit();
 			res.json({ message: "Wallets switched successfully" });
 		} else {
 			res.status(400).json({ error: "Main and second wallets not found" });
 		}
 	} catch (error) {
 		console.error(error);
-		await connection.rollback();
 		res.status(500).json({ error: "Internal server error" });
-	} finally {
-		connection.release();
 	}
 };
