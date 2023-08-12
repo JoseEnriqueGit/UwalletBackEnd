8fac793461c9135488db52b13389ca6fb50a4210
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-08-12 19:46:10 -0400

    change: Improved Validation and Error Handling in getUserWalletInformation
    Enhanced getUserWalletInformation with robust validation using Zod library. Validation errors are now accurately logged and returned to the client, improving error handling.

--- src/controllers/User/getUserWalletInformation.ts
+++ src/controllers/User/getUserWalletInformation.ts
@@ -3,6 +3,7 @@ import db from "../../config/data_base";
 import logger from "../../config/logger";
 import { BalanceRowSchema } from "../../models";
 import { balanceInfo } from "../../db";
+import { ZodError } from "zod";
 
 interface WalletResponse {
 	balance: number;
@@ -11,34 +12,32 @@ interface WalletResponse {
 	secondWalletCurrency: string;
 }
 
-export const getUserWalletInformation = async (
-	req: Request,
-	res: Response,
-) => {
+export const getUserWalletInformation = async (req: Request, res: Response) => {
 	try {
 		const result = await db.transaction(async (connection) => {
-			const walletsRows = await balanceInfo(connection, req.params.id);
+			const balanceInfoRows = await balanceInfo(connection, req.params.id);
 
-			if (!walletsRows || walletsRows.length === 0) {
+			if (!balanceInfoRows || balanceInfoRows.length === 0) {
 				return { message: "No wallets found" };
 			}
 
-			// Convert types of fields before validating with zod
-			const validatedWalletsRows = walletsRows.map((row) => {
-				// Convert is_main_wallet and is_second_wallet fields to boolean
-				row.is_main_wallet = !!row.is_main_wallet;
-				row.is_second_wallet = !!row.is_second_wallet;
-
-				// Convert balance field to number
-				row.balance = Number(row.balance);
-
-				return BalanceRowSchema.parse(row);
+			const validatedBalanceInfoRows = balanceInfoRows.map((row) => {
+				const result = BalanceRowSchema.safeParse(row);
+				if (result.success) {
+					return result.data;
+				} else {
+					const validationErrors = result.error.issues;
+					logger.error("Error retrieving user wallet information", {
+						validationErrors,
+					});
+					throw new ZodError(validationErrors);
+				}
 			});
 
-			const mainWallet = validatedWalletsRows.find(
+			const mainWallet = validatedBalanceInfoRows.find(
 				(walletRow) => walletRow.is_main_wallet
 			);
-			const secondWallet = validatedWalletsRows.find(
+			const secondWallet = validatedBalanceInfoRows.find(
 				(walletRow) => walletRow.is_second_wallet
 			);
 
@@ -58,10 +57,15 @@ export const getUserWalletInformation = async (
 			res.status(204).json({ message: "No wallets found" });
 		}
 	} catch (error) {
-		logger.error("Error retrieving user wallet information", { error });
-
-		res.status(500).json({
-			message: "An error occurred while retrieving user wallet information",
-		});
+		if (error instanceof ZodError) {
+			res
+				.status(400)
+				.json({ message: "Validation error", errors: error.issues });
+		} else {
+			logger.error("Error retrieving user wallet information", { error });
+			res.status(500).json({
+				message: "An error occurred while retrieving user wallet information",
+			});
+		}
 	}
 };
